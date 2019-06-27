b1425ffe51c8821f717aef95c022cd38e7096642
Manuel Tuero <manuel.tuero@wolox.com.ar>
2019-06-27 17:28:16 -0300

    chore: add a getAlbumById function

--- app/controllers/albums.js
+++ app/controllers/albums.js
@@ -8,6 +8,22 @@ exports.getAlbums = async (_, res) => {
     const albums = await response.json();
     res.status(200).send(albums);
   } catch (err) {
-    res.status(404).send(err.message);
+    res.status(500).send(err.message);
+  }
+};
+
+exports.getAlbumById = async (req, res) => {
+  try {
+    logger.info(`Fetching album with id ${req.params.id} from online store...`);
+    const response = await fetch(`https://jsonplaceholder.typicode.com/albums/${req.params.id}`);
+    const album = await response.json();
+    logger.info(JSON.stringify(album, null, 2));
+    if (Object.keys(album).length > 0) {
+      res.status(200).send(album);
+    } else {
+      res.status(404).send('Album not exists');
+    }
+  } catch (err) {
+    res.status(500).send(err.message);
   }
 };
--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,8 @@
 const { healthCheck } = require('./controllers/healthCheck');
-const { getAlbums } = require('./controllers/albums');
+const { getAlbums, getAlbumById } = require('./controllers/albums');
 
 exports.init = app => {
   app.get('/health', healthCheck);
   app.get('/albums', getAlbums);
+  app.get('/albums/:id', getAlbumById);
 };
