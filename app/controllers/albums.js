ba1f4c65f7d26d63ca718e5b8218e2a42587cd55
Manuel Tuero <manuel.tuero@wolox.com.ar>
2019-06-28 11:15:06 -0300

    feat: add a url for retrieve photos of album using id

--- app/controllers/albums.js
+++ app/controllers/albums.js
@@ -1,10 +1,13 @@
 const fetch = require('node-fetch');
 const logger = require('../logger');
 
+const ALBUMS_URL = 'https://jsonplaceholder.typicode.com/albums';
+const PHOTOS_URL = 'https://jsonplaceholder.typicode.com/photos';
+
 exports.getAlbums = async (_, res) => {
   try {
     logger.info('Fetching albums from online store...');
-    const response = await fetch('https://jsonplaceholder.typicode.com/albums');
+    const response = await fetch(ALBUMS_URL);
     const albums = await response.json();
     res.status(200).send(albums);
   } catch (err) {
@@ -15,13 +18,27 @@ exports.getAlbums = async (_, res) => {
 exports.getAlbumById = async (req, res) => {
   try {
     logger.info(`Fetching album with id ${req.params.id} from online store...`);
-    const response = await fetch(`https://jsonplaceholder.typicode.com/albums/${req.params.id}`);
+    const response = await fetch(`${ALBUMS_URL}/${req.params.id}`);
     const album = await response.json();
-    logger.info(JSON.stringify(album, null, 2));
     if (Object.keys(album).length > 0) {
       res.status(200).send(album);
     } else {
       res.status(404).send('Album not exists');
+    }
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+exports.getAlbumPhotos = async (req, res) => {
+  try {
+    logger.info(`Fetching the photos of album with id ${req.params.id} from online store...`);
+    const response = await fetch(`${PHOTOS_URL}?albumId=${req.params.id}`);
+    const photos = await response.json();
+    if (Object.keys(photos).length > 0) {
+      res.status(200).send(photos);
+    } else {
+      res.status(404).send('Not found');
     }
   } catch (err) {
     res.status(500).send(err.message);
