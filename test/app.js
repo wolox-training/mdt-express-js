e59814f4bdd372ac5dfe27e4befa1296fe0e949a
Manuel Tuero <manuel.tuero@wolox.com.ar>
2019-07-02 09:46:41 -0300

    feat: add drop and create npm scripts for database. add tests

--- test/app.js
+++ test/app.js
@@ -1,4 +1,25 @@
-describe('tests', () => {
-  // eslint-disable-next-line no-empty-function
-  test('empty', () => {});
+const { createUser } = require('../app/services/users'),
+  { userAlreadyExistsError } = require('../app/errors');
+
+const mockedUser = {
+  firstName: 'Manuel',
+  lastName: 'Tuero',
+  email: 'manuel.tuero@wolox.com.ar',
+  password: 'Wolox1189!'
+};
+
+describe('users api tests', () => {
+  test('createUser with valid input and the user does not exist creates correctly', () =>
+    expect(createUser(mockedUser)).resolves.toMatch(/manuel.tuero@wolox.com.ar/));
+
+  test('createUser with existing user failed creation', () => {
+    createUser(mockedUser);
+    const userWithExistingEmail = {
+      firstName: 'Foo',
+      lastName: 'bar',
+      email: 'manuel.tuero@wolox.com.ar',
+      password: 'Wolox1189!'
+    };
+    expect(createUser(userWithExistingEmail)).resolves.toThrow(userAlreadyExistsError);
+  });
 });
